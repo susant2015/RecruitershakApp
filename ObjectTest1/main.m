ed4d8e7a5e4e3db151f4bd61fcab5f96175117f3
admin <admin@admins-imac.local>
2016-04-11 10:58:29 +0530

    Initial Commit

---
+++ ObjectTest1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ObjectTest1
+//
+//  Created by admin on 11/04/16.
+//  Copyright © 2016 Xigmapro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
